ca4fe5295a667ef4da430dc593ca92d774408b87
MichalBlazek111183 <xblazekm1@stuba.sk>
2023-10-04 14:19:20 +0200

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -6,6 +6,27 @@ int
 main(int argc, char *argv[])
 {
 
+  int p_parent[2];
+  int p_child[2];
+
+  char buf;
+
+  pipe(p_parent);
+  pipe(p_child);
+
+  int pid = fork();
+  
+  if(pid == 0){
+    
+    read(p_parent[0], &buf,1);
+    printf("%d: received ping\n", getpid());
+    write(p_child[1],"b",1);
+  } else{
+    write(p_parent[1],"b",1);
+    
+    read(p_child[0], &buf,1);
+    printf("%d: received pong\n", getpid());
+  }
 
   exit(0);
 }
